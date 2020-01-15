58539bb54e2667353435b4c7b27a233d0ff00bf2
ryan_ <ahearn@hartford.edu>
2020-01-15 01:00:37 -0500

    Initial commit

---
+++ precache-manifest.a1dad045b01772d43336916e19659293.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1233158e4eebad79e50c1042f0c11cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cea9959bcc660c83c468",
+    "url": "/static/css/main.5427cb25.chunk.css"
+  },
+  {
+    "revision": "13082e8d3b56fcd8d9e3",
+    "url": "/static/js/2.4166d3f1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.4166d3f1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cea9959bcc660c83c468",
+    "url": "/static/js/main.0efc7322.chunk.js"
+  },
+  {
+    "revision": "cb3391e0e4e8ed642cc6",
+    "url": "/static/js/runtime-main.797c8b25.js"
+  }
+]);
